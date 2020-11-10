7cdb1e885b2ecda0cdb140ff40bbb02ab83d63e6
annieruth14 <ruthannie3@gmail.com>
2020-11-10 10:24:51 +0530

    [Annie]Added: code to calculate wage given condition using for loop

--- employeeWage.js
+++ employeeWage.js
@@ -29,7 +29,15 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC3 - Emp Wage: "+empWage + " for hours: "+ empHrs);
+
+// UC4
+{
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC4 - Total Hours: "+ totalEmpHrs+ " Emp Wage: "+ empWage);
+}
